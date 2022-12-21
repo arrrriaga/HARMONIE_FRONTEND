867ecedfd78f024f20c12a5220d68dc729465262
Francisco Arriaga Velasco <94820722+arrrriaga@users.noreply.github.com>
2022-12-21 05:58:46 -0600

    Cartas centradas

--- src/pages/ProductosPage/index.js
+++ src/pages/ProductosPage/index.js
@@ -1,13 +1,12 @@
 import { useEffect, useContext } from "react";
 import { ProductContext } from "../../context/Product.context";
 import { verProductos } from "../../services";
-import { Card, Button, Row } from "react-bootstrap";
+import { Card, Button, Row, Col, CardGroup } from "react-bootstrap";
 import "./style.css";
 import { UserContext } from "../../context/User.context";
 import { NavLink } from "react-router-dom";
 
 const ProductosPage = () => {
-  const columns = 4;
   const { productos, guardarProductos, agregarACarrito } =
     useContext(ProductContext);
 
@@ -24,59 +23,57 @@ const ProductosPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <Row>
-      {productos.map((producto, index) => (
-        <Card
-          style={{ margin: "15px" }}
-          key={index}
-          className={`col-${12 / columns}`}
-        >
-          <Card.Img className="imgCard" variant="top" src={producto.img} />
-          <Card.Body>
-            <Card.Title>
-              {producto.nombre} {producto.ml}
-            </Card.Title>
-            <Card.Text>{producto.description}</Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <h5>${producto.price}</h5>
-            <NavLink
-              className="btn btn-info"
-              to={`/products/detalles/${producto._id}`}
-            >
-              Ver mas...
-            </NavLink>
-            {tipo === "admin" ? (
-              <Button
-                variant="danger"
-                onClick={() => {
-                  agregarACarrito({
-                    nombre: producto.nombre + producto.ml,
-                    price: producto.price,
-                    _id: producto._id,
-                  });
-                }}
+    <CardGroup>
+      <Row xs={1} md={3} className="g-4">
+        {productos.map((producto, index) => (
+          <Card key={index}>
+            <Card.Img className="imgCard" variant="top" src={producto.img} />
+            <Card.Body>
+              <Card.Title>
+                {producto.nombre} {producto.ml}
+              </Card.Title>
+              <Card.Text>{producto.description}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <h5>${producto.price}</h5>
+              <NavLink
+                className="btn btn-info"
+                to={`/products/detalles/${producto._id}`}
               >
-                Eliminar producto
-              </Button>
-            ) : (
-              <Button
-                variant="success"
-                onClick={() => {
-                  agregarACarrito({
-                    nombre: producto.nombre + producto.ml,
-                    price: producto.price,
-                    _id: producto._id,
-                  });
-                }}
-              >
-                Add to cart
-              </Button>
-            )}
-          </Card.Footer>
-        </Card>
-      ))}
-    </Row>
+                Ver mas...
+              </NavLink>
+              {tipo === "admin" ? (
+                <Button
+                  variant="danger"
+                  onClick={() => {
+                    agregarACarrito({
+                      nombre: producto.nombre + producto.ml,
+                      price: producto.price,
+                      _id: producto._id,
+                    });
+                  }}
+                >
+                  Eliminar producto
+                </Button>
+              ) : (
+                <Button
+                  variant="success"
+                  onClick={() => {
+                    agregarACarrito({
+                      nombre: producto.nombre + producto.ml,
+                      price: producto.price,
+                      _id: producto._id,
+                    });
+                  }}
+                >
+                  Add to cart
+                </Button>
+              )}
+            </Card.Footer>
+          </Card>
+        ))}
+      </Row>
+    </CardGroup>
   );
 };
 
