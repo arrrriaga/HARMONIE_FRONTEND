e1b87dd9ac6eeef2378d05315bbe1be23c407b11
Francisco Arriaga Velasco <94820722+arrrriaga@users.noreply.github.com>
2022-12-21 16:47:28 -0600

    Navbar mejorado

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,35 +5,51 @@ import { UserContext } from "../context/User.context";
 import "./style.css";
 import TLALLI from "../Images/TLALLI.png";
 import { clienteRoutes, adminRoutes, publicRoutes } from "../routes";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const NavComponent = () => {
   const { tipo, token } = useContext(UserContext);
-
+  const expand = "sm";
+  const expand2 = "lg";
   return (
-    <Navbar className="NAV" variant="dark" expand="sm">
-      <Container>
-        <Navbar.Brand href="/">
-          <img src={TLALLI} alt="TLALLI" height={50}></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {(token
-              ? tipo === "admin"
-                ? adminRoutes
-                : clienteRoutes
-              : publicRoutes
-            )
-              .filter((route) => route.title)
-              .map((route, index) => (
-                <Nav.Link as={Link} to={route.path} key={index}>
-                  {route?.title}
-                </Nav.Link>
-              ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar key={expand} variant="dark" expand={expand} className="NAV mb-3">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img src={TLALLI} alt="TLALLI" height={50}></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand2}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+            style={{ width: "30vw", background: "#c1cdaa" }}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Menú
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-1">
+                {(token
+                  ? tipo === "admin"
+                    ? adminRoutes
+                    : clienteRoutes
+                  : publicRoutes
+                )
+                  .filter((route) => route.title)
+                  .map((route, index) => (
+                    <Nav.Link as={Link} to={route.path} key={index}>
+                      {route?.title}
+                    </Nav.Link>
+                  ))}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
